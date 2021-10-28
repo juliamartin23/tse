ef85290c071203a959275ffea6c1cccfed21df07
Julia Martin <jlmartin42@icloud.com>
2021-10-28 12:04:17 -0400

    step 4

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -19,13 +19,15 @@
 #define TABLESIZE 10 
 #define MAXREG 100 
 
+static int sum = 0; 
+
 typedef struct helement { 
 	char word[MAXREG]; 
 	queue_t *qt; 
 } helement_t; 
 
 typedef struct qelement { 
-	char documentid[MAXREG]; 
+	int documentid;  
 	int count; 
 	struct qelement *next; 
 } qelement_t; 
@@ -41,7 +43,7 @@ helement_t *make_hel(char *word){
 	return pp; 
 }
 
-qelement_t *make_qel(char *documentid, int count){
+qelement_t *make_qel(int documentid, int count){
 	qelement_t *pp = (qelement_t*)malloc(sizeof(qelement_t)); 
 	if(!pp){
 		printf("Error: malloc failed allocating element\n");
@@ -49,7 +51,7 @@ qelement_t *make_qel(char *documentid, int count){
 	}
 	pp->next = NULL; 
 	pp->count = count; 
-	strcpy(pp->documentid, documentid); 
+	pp->documentid = documentid; 
 	return pp; 
 }
 
@@ -74,7 +76,41 @@ bool searchpage (void *elementp, const void *keyp){
 bool searchfn (void *elementp, const void *keyp){
     qelement_t* qp = (qelement_t *)elementp;
     int* key = (int *)keyp;
-    return((qp->documentid) == key);
+    return((qp->documentid) == *(int*) key);
+}
+
+// static void printel(queue_t *p) {
+// 	queue_t* qp = (queue_t *) p; 
+// 	printf("qel: %d\n %d\n", qp->documentid, qp->count); 
+// }
+
+// void freeq(void *elementp){
+// 	qelement_t* qel = (qelement_t *)elementp;
+// 	qclose(qel); 
+
+// }
+// void freeh(void *elementp){
+// 	helement_t* hel = (helement_t *)elementp; 
+// 	queue_t *q = (queue_t *) hel->qt; 
+
+// 	qapply(q, freeq); 
+// }
+
+
+void sumq(void *elementp){
+	qelement_t* qel = (qelement_t *)elementp; 
+	sum = sum + qel->count; 
+	printf("this sum: %d \n", sum); 
+	//qclose(qel); 
+	//free(qel);
+
+}
+
+void sumwords(void *elementp) {
+	helement_t* hel = (helement_t *)elementp; 
+	queue_t *q = (queue_t *) hel->qt; 
+
+	qapply(q, sumq); 
 }
 							
 int main(void){
@@ -84,13 +120,20 @@ int main(void){
  	webpage_t *page; 
 	int pos=0;
 	char *word;
-	int sum = 0; 
 
 	hashtable_t *htable = hopen(TABLESIZE); 
 	
 
 	while((page=pageload(id, dirnm)) != NULL) { 
 		while((pos=webpage_getNextWord(page, pos, &word))>0) {
+			printf("id: %d\n", id);
+			char* url = webpage_getURL(page); 
+			int depth = webpage_getDepth(page);  
+			int len = webpage_getHTMLlen(page); 
+			queue_t *qt;
+
+			printf("%s\n%d\n%d\n", url, depth, len); 
+
 			if (strlen(word) > 2 && NormalizeWord(word)) {
 				printf("%s\n", word);
 
@@ -101,13 +144,15 @@ int main(void){
 					//qput this queue element into the queue 
 					//make queue element next = NULL  
 					//put new hash element into hash table 
+					printf("word not in hash\n");
 					helement_t *hel = make_hel(word);
-					queue_t *qt = qopen(); 
+					qt = qopen(); 
 					qelement_t *qel = make_qel(id, 1); 
 					qput(qt, qel);  
+					//qapply(qt, printel);
 					hel->qt = qt; 
 					hput(htable, hel, word, strlen(word)); 
-					sum++; 
+					
 				}
 				else {
 					//find last element of queue 
@@ -119,35 +164,48 @@ int main(void){
 					
 					qelement_t *qel; 
 					//if current doc id already exists in the queue, just increment the count 
-					if((qel = qsearch(qt, searchfn, id)) != NULL ) { 
+					if((qel = qsearch(qt, searchfn, &id)) != NULL ) { 
 						qel->count = qel->count+1; 
+						printf("word in hash, id in queue, just increment\n");
+					
 					}
 					// if there is no queue element with curent doc id, make a new one 
 					else{
 						qelement_t *qel = make_qel(id, 1); 
 						qput(qt, qel); 
+						printf("in hash, but no doc id in queue\n");
 					}
 				}
 
 			}
-			free(word);
-			webpage_delete(page); 
+			//free(word);
+			//webpage_delete(page); 
+		
 		}
-		//webpage_delete(page); 
-		
+		//printf("incrementing id\n");
 		id++; 
+        webpage_delete(page);
+		//free(word);
 	} 
+	happly(htable, sumwords); 
 
-	//happly(table, qclose(hel->qt)); 
-	
-	//printf("Sum of table is: %d\n", sum); 
-	//go through all the queue elements and call qclose(); 
-	//for(i=0;i < len(table); i++)){
-		
-	//}
+	id = 1; 
+	while((page=pageload(id, dirnm)) != NULL) { 
+		while((pos=webpage_getNextWord(page, pos, &word))>0) {
+
+			helement_t *hel = hsearch(htable, searchpage, word, strlen(word)); 
+			queue_t *qt = hel->qt; 
+			free(word); 
+			qclose(qt); 
+		}
+		id++; 
+		//webpage_delete(page); 
+	}
+
 	hclose(htable); 
 
-	
- 
+	printf("SUM: %d\n", sum); 
+	hclose(htable); 
+
 	return 0;
 }
