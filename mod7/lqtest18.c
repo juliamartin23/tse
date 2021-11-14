0356e0223ca1e8def121c93aef933afe16041e61
ezgiokutan <ezgi.okutan.22@dartmouth.edu>
2021-11-14 15:26:00 -0500

    step 1 done

--- mod7/lqtest18.c
+++ mod7/lqtest18.c
@@ -8,6 +8,9 @@
 
 #define MAXREG 10
 
+int num = 4; 
+int index = 2000; 
+
 typedef struct car {
 	struct car *next;
 	char plate[MAXREG];
@@ -20,21 +23,14 @@ typedef struct argstruct {
 	car_t *car;
 } arg_t;
 
-
-static void printcar(void *p) {
-	car_t* cp = (car_t *)p;
-	printf("Plate : %s, Price: %f, Year: %d\n", cp->plate, cp->price, cp->year);
-}
-
-car_t *front=NULL;
+typedef struct putstruct {
+	lqueue_t *queue; 
+	int iterator; 
+} put_t; 
 
 car_t *make_car(char *plate,double price,int year)  { 
 	car_t *pp;
 	pp = (car_t*)malloc(sizeof(car_t));
-	//if(!(pp = (car_t*)malloc(sizeof(car_t)))) {
-		//printf("[Error: malloc failed allocating car]\n");
-		//return NULL;
-	//}
 	pp->next = NULL;
 	strcpy(pp->plate,plate);
 	pp->price=price;
@@ -48,8 +44,68 @@ bool searchfn (void *elementp, const void *keyp){
     return(cp->year == *key);
 }
 
+static void printcar(void *p) {
+	car_t* cp = (car_t *)p;
+	printf("Plate : %s, Price: %f, Year: %d\n", cp->plate, cp->price, cp->year);
+}
+
+void createthread (pthread_t threadarr[],void*(fn)(void*arg), int number, void* item){
+
+	for(int i=0; i<number; i++){
+		int error = pthread_create(&threadarr[i],NULL,fn, item); 
+		if(error){
+			printf("%d: creation unsuccessful", error); 
+			exit(EXIT_FAILURE);   
+		}  	      	
+	}
+
+	for(int i=0; i<number; i++){
+		int error = pthread_join(threadarr[i],NULL); 
+		if(error){
+			printf("%d: creation unsuccessful", error); 
+			exit(EXIT_FAILURE);   
+		}  	      	
+	}				
+}
+
+
+void* gettingfn(void* arg){
+	lqueue_t *lqt = (lqueue_t*) arg; 
+	sleep(3); 
+	car_t* car; 
+	while((car= lqget(lqt))!=NULL){
+		printf("Removing plate : %s, Price: %f, Year: %d\n", car->plate, car->price, car->year);
+		free(car); 
+	}
+	return NULL; 
+}
+
+void* puttingfn(void* arg){
+	lqueue_t *lqt = (lqueue_t*) arg; 
+	while(num!=0){
+		car_t *car = make_car("plate", 3000, index++); 
+		lqput(lqt, car); 
+		num--; 
+	}
+	return NULL; 
+}
+
+void* searchingfn(void* arg){
+	lqueue_t *lqt = (lqueue_t*) arg; 
+	car_t* car; 
+	if((car = lqsearch(lqt, searchfn, &index))!=NULL){
+		printf("car with year %d found\n", index); 
+		printf("Car-> "); 
+		printcar(car); 
+	} 
+	return NULL; 
+}
+
+car_t *front=NULL;
+
 int main(void){
-  	pthread_t tid1,tid2,tid3;
+	int numt = 2; 
+  	pthread_t threadarray[numt]; 
     lqueue_t *lqp = lqopen(); 
     //car_t *queuep= qopen();
     
@@ -64,76 +120,23 @@ int main(void){
     lqapply(lqp, printcar);
     printf("....\n");
 
-	// void *carp1= lqget(lqp);
-	// car_t* cp1 = (car_t *)carp1;
-
-	// if(cp1==NULL){
-	// 	printf("got car\n");
-	// 	lqclose(lqp);
-	// 	printcar(cp1);
-	// 	free(cp1);
-	// 	exit(EXIT_FAILURE);
-	// }
-
-	// const int key= 2007;  
-	// void *foundCar = lqsearch(lqp, searchfn, (void *)&key);
-	// car_t* fCar = (car_t *)foundCar;
-
-	// if(fCar==NULL){
-	// 	printf("found car\n");
-	// 	printcar(fCar);
-	// 	lqclose(lqp);
-	// 	exit(EXIT_FAILURE);
-	// }
-	// printf("got car\n");
-	// printcar(cp1);
-	
-	// printf("....\n");
-
-	// printf("found car\n");
-	// printcar(fCar);
-
-	// printf("....\n");
-	// printf("final queue\n");
-	// lqapply(lqp, printcar);
-
-	arg_t *argument;
-	car_t *carp5;
-
-	//argument->queue= lqp
-
-	if((pthread_create(&tid1,NULL,lqget,(void *)lqp))!=0) {  
-	exit(EXIT_FAILURE);             	
-}					
-
-	if((pthread_create(&tid2,NULL,lqget,(void *)lqp))!=0) {  
-	exit(EXIT_FAILURE);             	
-}		
-
-
-	if((pthread_create(&tid3,NULL,lqget,(void *)lqp))!=0) {  
-	exit(EXIT_FAILURE);             	
-}		
-
- 
-if((pthread_join(tid3,NULL))!=0) {  
-	exit(EXIT_FAILURE);             	
-}		
-	printf("after join thread 3\n");
-
-if(((pthread_join(tid2,NULL))!=0)) {   
-	exit(EXIT_FAILURE);             	
-}		
-	printf("after join thread 2\n");
-
-if(((pthread_join(tid1,NULL))!=0)) {   
-	exit(EXIT_FAILURE);             	
-}		
-	printf("after join thread 1\n");
+	createthread(threadarray,gettingfn, numt,lqp); 
 
 	printf("final \n");
 	lqapply(lqp,printcar);
 	lqclose(lqp);
+
+	printf("Testing lqput: \n");
+	lqueue_t *lqp2 = lqopen(); 
+	createthread(threadarray,puttingfn, numt,lqp2); 
+	printf("final \n");
+	lqapply(lqp2,printcar);
+	index = 2001; 
+	createthread(threadarray,searchingfn, numt,lqp2);
+	lqclose(lqp2);
+
 	//free(cp1);
     exit(EXIT_SUCCESS); 
+
 }
+
